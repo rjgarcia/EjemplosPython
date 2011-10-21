d7113563f498f415633e25eadda73476c62f3671
Rafael J Garcia <lestatmer@gmail.com>
2011-10-21 09:34:08 +0200

    Subido Manolito

---
+++ hola_manolito.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+print "Hola Manolito"
+
+lista=[1,2,3,[10,11]]
+
+
+
+#Listas
+
+'''print "/n" 
+
+print lista
+
+print "Primera Posición: " + str(lista [3][0])
+
+print "Rango:0 a 3 -: " + str (lista [0:3])
+
+print "Rango:1 en adelante-: " + str (lista [1:])
+
+#lista[1:2]= [10,10]
+
+print lista[::4]'''
+
+#diccionarios
+
+dicc={"Barcelona" : "Mesi",
+	  "Real Madrid" : "Ronaldo"}
+	  
+
+print dicc
+
+print dicc["Barcelona"]
+
+
+print "Qué bonito!!!!!!!!!!!! "
+
+raw_input()
+
